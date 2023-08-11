f542981296959e0a35b05d17efc7dc9576689d76
Rahulsingh912 <rahulkiit1234@gmail.com>
2023-08-11 23:25:24 +0530

    first commit

---
+++ server/midldlewares/auth.js
@@ -0,0 +1,54 @@
+// import mongoose from 'mongoose';
+// import User from '../models/auth.js'
+
+// export const getAllUsers = async (req, res) => {
+//     try {
+//         const allUsers = await User.find();
+//         const allUserDetails = []
+//         allUsers.forEach(users => {
+//             allUserDetails.push({ _id: users._id, name: users.name, about: users.about, tags: users.tags, joinedOn: users.joinedOn })
+//         })
+//         res.status(200).json(allUserDetails);
+//     } catch (error) {
+//         res.status(404).json({ message: error.message });
+//     }
+// }
+
+// export const updateProfile = async (req, res) => {
+//   const { id: _id } = req.params;
+//   const { name, about, tags } = req.body;
+
+//   if (!mongoose.Types.ObjectId.isValid(_id)) {
+//     return res.status(404).send("question unavailable...");
+//   }
+
+//   try {
+//     const updatedProfile = await users.findByIdAndUpdate(
+//       _id,
+//       { $set: { name: name, about: about, tags: tags } },
+//       { new: true }
+//     );
+//     res.status(200).json(updatedProfile);
+//   } catch (error) {
+//     res.status(405).json({ message: error.message });
+//   }
+// };
+
+
+
+import jwt from 'jsonwebtoken'
+
+const auth = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+
+        let decodeData = jwt.verify(token, process.env.JWT_SECRET)
+        req.userId = decodeData?.id 
+
+        next()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default auth;
